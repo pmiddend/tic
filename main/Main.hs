97aabb9d965f90cf0ad6e752f60baf538ab786c6
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-06-24 22:22:50 +0200

    First version showing the colored world map

--- main/Main.hs
+++ main/Main.hs
@@ -1,9 +1,14 @@
 module Main where
 
 import Wrench.MonadGame
+import Wrench.SpriteIdentifier
+import Wrench.Point
 import Wrench.Platform
+import Wrench.Picture
 import ClassyPrelude 
 import Wrench.Rectangle
+import Data.Maybe
+import Wrench.RenderPositionMode
 import Control.Lens((^.))
 import Linear.V2
 
@@ -16,31 +21,40 @@ fitRectAspectPreserving outer inner =
   then
     let
       width = outer ^. rectangleDimensions . _x
-      smallerRatio = inner ^. rectangleDimensions . _x / inner ^. rectangleDimensions . _y
+      smallerRatio = inner ^. rectangleDimensions . _y / inner ^. rectangleDimensions . _x
       height = width * smallerRatio
       x = 0
       y = outer ^. rectangleDimensions . _y / 2 - height / 2
     in
-      rectangleFromPoints (V2 x y) (V2 width height)
+      rectangleFromOriginAndDim (V2 x y) (V2 width height)
   else
     let
       height = outer ^. rectangleDimensions . _y
-      smallerRatio = inner ^. rectangleDimensions . _y / inner ^. rectangleDimensions . _x
+      smallerRatio = inner ^. rectangleDimensions . _x / inner ^. rectangleDimensions . _y
       width = height * smallerRatio
       x = outer ^. rectangleDimensions . _x / 2 - width / 2
       y = 0
     in
-      rectangleFromPoints (V2 0 0) (V2 width height)
+      rectangleFromOriginAndDim (V2 x y) (V2 width height)
 
+mapImageId :: SpriteIdentifier
+mapImageId = "map"
 
-mapWidth = 3600
+originRectangle :: Wrench.Point.Point -> Rectangle
+originRectangle = rectangleFromPoints (V2 0 0)
 
-mapHeight = 1800
+picturize :: OuterRectangle -> InnerRectangle -> Picture
+picturize viewportRectangle mapSize =
+  let fitRect = fitRectAspectPreserving viewportRectangle mapSize
+  in (fitRect ^. rectLeftTop) `pictureTranslated` (pictureSpriteResampled mapImageId RenderPositionTopLeft (fitRect ^. rectangleDimensions))
 
 mainLoop = do
   events <- gpollEvents
   gupdateTicks 1.0
   gupdateKeydowns events
+  viewport <- gviewportSize
+  mapImage <- fromJust <$> (glookupImageRectangle mapImageId)
+  grender (picturize (originRectangle viewport) mapImage)
   mainLoop
 
 main :: IO ()
